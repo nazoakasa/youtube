29f9155e79129d48f22fdb4e11a83b78f17cee93
nazoakasa <151802644+nazoakasa@users.noreply.github.com>
2025-08-26 20:59:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ app.get("/video", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(80, () => console.log("Server running on port 80"));
